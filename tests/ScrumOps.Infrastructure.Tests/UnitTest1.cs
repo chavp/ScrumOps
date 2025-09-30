335b0b90587904934a5316ca0d9cfaa94e0048bb
Chavp <my.parinya@gmail.com>
2025-09-30 15:05:33 +0700

    add database

--- tests/ScrumOps.Infrastructure.Tests/UnitTest1.cs
+++ tests/ScrumOps.Infrastructure.Tests/UnitTest1.cs
@@ -2,6 +2,8 @@ namespace ScrumOps.Infrastructure.Tests;
 
 public class UnitTest1
 {
+    // docker run --name scrumops-postgres -e POSTGRES_PASSWORD=scrumops888 -d -p 5432:5432 postgres
+
     [Fact]
     public void Test1()
     {
